0ce035e2d790451318926e6b5251a611cf0dc1c5
Kavini-hub <141908913+Kavini-hub@users.noreply.github.com>
2024-02-01 21:06:11 +0530

    2/1 kavini

--- src/Dapproval.js
+++ src/Dapproval.js
@@ -1,17 +1,17 @@
 import Patienthead from './Components/Patienthead';
 import Dapproval01 from './Dapproval01';
 import './maincss/Selecttest.css'
-import BackToTop from './Components/Scroll';
+
 import Footer from './Components/Footer';
-import Date from './Components/Date';
+
 function Dapproval() {
     return (
       <div className="Selecttest">
        
         <Patienthead/><br/><br/><br/><br/><br/>
         <Dapproval01/>
-        <BackToTop />
-        <Date/><br/>
+        
+        <br/>
         <Footer/>
       </div>
       
--- src/Dapproval01.js
+++ src/Dapproval01.js
@@ -6,7 +6,7 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-
+import Dapproval02 from './Dapproval02';
 
 export default function BasicSelect() {
   const [test, setTest] = React.useState('');
@@ -17,6 +17,7 @@ export default function BasicSelect() {
 
   return (
     <Box sx={{ width: '80%', margin: 'auto', backgroundColor: '#D9D9D9', padding: '20px' ,borderRadius: '8px'}}>
+      <Dapproval02/>
       <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label" sx={{color:'#101754'}}>Report ID</InputLabel>
         <Select
@@ -47,8 +48,11 @@ export default function BasicSelect() {
         <Button sx={{variant:'contained' ,color:'#FFFFFF', background:'#101754'}}>
           Recommend to recheck
         </Button>
+        
       </Box>
       
     </Box>
+    
   );
+  
 }
