b9480a3eed751785b03d8731997e2ac1519e07e1
Kavini-hub <141908913+Kavini-hub@users.noreply.github.com>
2024-02-06 20:02:25 +0530

    02/06

--- src/Dapproval01.js
+++ src/Dapproval01.js
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Dapproval02 from './Dapproval02';
+import Grid from '@mui/material/Grid';
 
 export default function Dapproval01() {
   const [test, setTest] = React.useState('');
@@ -14,42 +12,36 @@ export default function Dapproval01() {
     setTest(event.target.value);
   };
 
+  
   return (
     <Box sx={{ width: '80%', margin: 'auto', backgroundColor: '#D9D9D9', padding: '20px' ,borderRadius: '8px'}}>
+       <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+      <Grid item xs={2}>
       <Dapproval02/>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label" sx={{color:'#101754'}}>Report ID</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={test}
-          label="Blood Test Type"
-          onChange={handleChange}
-          sx={{ backgroundColor: '#FFFFFF' }}
+      </Grid>
+      <Grid item xs={6}>
 
-        >
-          
-        </Select>
-      </FormControl>
-      
-      {test && (
-        <Typography variant="subtitle1" sx={{ marginTop: '10px' }}>
-          
-           {`${test}`}
-           
-        </Typography>
-      )}
-      <Box sx={{ marginLeft: 'auto', marginTop: '10px' }}>
-        {/* Add your button component here */}
-        <Button sx={{variant:'contained' ,color:'#FFFFFF', background:'#101754'}}>
+      <TextField
+         Report ID
+          id="outlined-required"
+          label="Report Id"
+        />
+        </Grid>
+        <Grid item xs={1}>
+         <Button sx={{variant:'contained' ,color:'#FFFFFF', background:'#101754',height:'50px'}}>
           Approve
-        </Button>&nbsp;
-        <Button sx={{variant:'contained' ,color:'#FFFFFF', background:'#101754'}}>
+        </Button>
+        </Grid>
+        <Grid item xs={1}>
+        <Button sx={{variant:'contained' ,color:'#FFFFFF', background:'#101754',width:'250px',height:'50px'}}>
           Recommend to recheck
         </Button>
         
-      </Box>
-      
+        </Grid>
+        </Grid>
+        </Box>
+
     </Box>
     
   );
--- src/Dapproval03.js
+++ src/Dapproval03.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 
 export default function FixedContainer() {
@@ -10,6 +11,12 @@ export default function FixedContainer() {
      
         <Box sx={{  width: '80%', margin: 'auto', backgroundColor: '#D9D9D9', padding: '20px' ,borderRadius: '8px',height:'50vh'}} >
             <h1>Recommendations</h1>
+            <hr></hr>
+            <TextField
+         Report ID
+          id="outlined-required"
+          label="Report Id"
+        />
             </Box>
      
     </React.Fragment>
