c676d3521549c0468b84bddf3bb07911fe8b991b
Kavini-hub <141908913+Kavini-hub@users.noreply.github.com>
2024-03-21 22:23:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Selecttest from './Selecttest';
 import AddTest from './Admin/AddTest';
 import AdminInterface from './Admin/AdminInterface';
+import Dhome from './Dhome';
+import Dapproval from './Dapproval';
+import Contact from './Contact';
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
       <Route path='AdminInterface' element={<AdminInterface/>}/>
       <Route path='Selecttest' element={<Selecttest/>}/>
       <Route path='AddTest' element={<AddTest/>}/>
+      <Route path='Dhome' element={<Dhome/>}/>
+      <Route path='Dapproval' element={<Dapproval/>}/>
+      <Route path='Contact' element={<Contact/>}/>
+    
     </Routes>
   </BrowserRouter>
   );
