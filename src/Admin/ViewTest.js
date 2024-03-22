62f33126fdf180242fd65447b32d04754a1fbcd7
chamodlw <chamodweerasinghe@gmail.com>
2024-03-22 16:51:49 +0530

    3/21

--- src/Admin/ViewTest.js
+++ src/Admin/ViewTest.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import Testlist from '../Components/Testlist'
+import Testlist from '../Components/Testlist';
 import Patienthead from '../Components/Patienthead'; // Importing header component
 import Footer from '../Components/Footer'; // Importing footer component
 import { Grid } from '@mui/material';
 
 function ViewTest() {
-  
   return (
-    <div>
-        <Patienthead/>
-        <Grid sx={{backgroundColor:'#D9D9D9'}}>
-        <br/><br/><br/><br/><br/><br/>
-        
-        <Testlist/>
-        
-        <br/><br/>
-        </Grid>
-        <Footer/>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Patienthead />
+      <Grid sx={{ backgroundColor: '#a6a6a6', flex: 1 }}>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Testlist />
+        <br />
+        <br />
+      </Grid>
+      <Footer />
     </div>
   );
 }
