b58f6c6eb942f5ad271a5362dff8ad5b0154a330
TheodaVidyani <theodahettiarachchi00@gmail.com>
2024-03-26 21:32:52 +0530

    03/26

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,112 +1,70 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import photo1 from '../images/HealthLabLogo.jpg';
+import { Grid, Typography, TextField } from "@mui/material";
+import photo1 from "../images/HealthLabLogo.jpg";
 import photo2 from "../images/BloodDraw.webp";
 
 function Login() {
-  //return
   return (
-    <div>
-      
+    <Grid container justifyContent="center">
       <form
         style={{
-          display: "flex",
           borderRadius: "15px",
           padding: "20px 40px",
           backgroundColor: "#D3E9FE",
           width: "65%",
-          marginLeft: "15%",
           marginTop: "10%",
           boxShadow: "1px 5px 3px -3px rgba(0,0,0,0.44)",
+          display: "flex",
         }}
       >
-        
-        {/* Photo2 on the left */}
-        <div className="form-group">
+        {/* Left Column with Photo2 */}
+        <Grid item xs={6}>
           <img
             src={photo2}
             style={{ width: "100%", maxWidth: "450px", marginTop: "15px" }}
-            alt="My Photo"
+            alt="Blood Draw Photo"
           />
-        </div>
+        </Grid>
 
-
-        {/* Form content on the right */}
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            marginLeft: "50px", 
-          }}
-        >
-
-          
-<img
-          src={photo1} // Use the imported image
-          // alt="My Photo"
-          style={{ width: "100%", maxWidth: "300px", marginTop: "10px", marginLeft: "100px"}}
-        />
-
-          <h2
-            style={{
-              marginBottom: "7%",
-              color: "#0085FF",
-              fontWeight: "bold",
-              
-            }}
-          >
-
-            Login
-          </h2>
-
-          <div className="form-group"  style={{ marginBottom: "25px" }}>
-            <label className="labels" htmlFor="username">
-              Username
-            </label>
-            <div className="custom_input">
-              <input
-                style={{
-                  width: "350px",
-                  height: "25px",
-                  borderRadius: "5px",
-                  background: "rgba(0, 0, 0, 0.04)",
-                }}
-                className="details-input form-control"
-                type="text"
-                id="username"
-                placeholder=""
-              />
-            </div>
-          </div>
-
-
-          <div className="form-group"  style={{ marginBottom: "25px" }}>
-            <label className="labels" htmlFor="password">
-              Password
-            </label>
-            <div className="custom_input">
-              <input
-                style={{  
-                  width: "350px",
-                  height: "25px",
-                  borderRadius: "5px",
-                  background: "rgba(0, 0, 0, 0.04)",
-                }}
-                className="details-input form-control"
-                type="text"
-                id="password"
-                placeholder=""
-              />
-            </div>
-          </div>
-
-          <div className="form-group"  style={{ marginBottom: "20px" }}>
-            <label
-              className="labels"
-              htmlFor="space"
+        {/* Right Column with Form Content */}
+        <Grid item xs={6} style={{ paddingLeft: "50px" }}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <img
+              src={photo1}
+              style={{ width: "100%", maxWidth: "300px", marginTop: "10px", marginLeft: "100px" }}
+              alt="HealthLab Logo"
+            />
+            <Typography
+              variant="h5"
               style={{
+                marginBottom: "7%",
+                color: "#0085FF",
+                fontWeight: "bold",
+              }}
+            >
+              Login
+            </Typography>
+
+            <TextField
+              fullWidth
+              label="Username"
+              variant="outlined"
+              style={{ marginBottom: "25px" }}
+            />
+
+            <TextField
+              fullWidth
+              label="Password"
+              variant="outlined"
+              style={{ marginBottom: "25px" }}
+            />
+
+            <Typography
+              variant="body1"
+              style={{
+                marginBottom: "20px",
                 fontFamily: "Inter",
-                fontStyle: "normal",
                 fontWeight: "500",
                 fontSize: "12px",
                 lineHeight: "24px",
@@ -114,28 +72,19 @@ function Login() {
               }}
             >
               Forgot Password?
-            </label>
-          </div>
+            </Typography>
 
-          <div className="form-group">
-            {/* Additional labels or form elements can be added here */}
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ width: "150px", height: "35px", borderRadius: "10px" }}
+            >
+              Login
+            </Button>
           </div>
-
-          <Button
-            sx={{
-              variant: "contained",
-              color: "#FFFFFF",
-              background: "#101754",
-              width: "150px",
-              height: "35px",
-              borderRadius: "10px",
-            }}
-          >
-            Login
-          </Button>
-        </div>
+        </Grid>
       </form>
-    </div>
+    </Grid>
   );
 }
 
--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -1,264 +1,104 @@
 import React from "react";
 import Button from '@mui/material/Button';
 import photo from '../images/HealthLabLogo.jpg';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Login from './Login';
-
+import { Link } from 'react-router-dom';
+import { Grid, Typography, TextField } from '@mui/material';
 
 function Signin() {
   return (
-
-    <div>
-      <div>
-
-      </div>
+    <Grid container justifyContent="center">
       <form
         style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr", // Two columns
           borderRadius: "15px",
           padding: "20px 40px",
           backgroundColor: "#D3E9FE",
           width: "65%",
-          marginLeft: "15%",
-          marginTop: "10%",
-          boxShadow:"1px 5px 3px -3px rgba(0,0,0,0.44)"
+          boxShadow: "1px 5px 3px -3px rgba(0,0,0,0.44)"
         }}
       >
+        <Grid container justifyContent="center">
+          <img
+            src={photo}
+            style={{ width: "100%", maxWidth: "300px", marginTop: "15px", marginBottom: "25px" }}
+            alt="HealthLab Logo"
+          />
+        </Grid>
+        <Typography variant="h5" align="center" style={{ marginBottom: "5%", fontSize: "30px", color: "#0085FF", fontWeight: "bold" }}>
+          Sign In
+        </Typography>
 
-        <img
-          src={photo} // Use the imported image
-          // alt="My Photo"
-          style={{ width: "100%", maxWidth: "300px", marginTop: "15px" }}
-        />
-        <h2
-          style={{
-            gridColumn: "span 2", // Span across two columns
-            marginBottom: "5%",
-            marginLeft: "45%",
-            color: "#0085FF",
-            fontWeight: "bold",
-          }}
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="Full Name"
+              variant="outlined"
+              style={{ marginBottom: "20px" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="Email"
+              variant="outlined"
+              style={{ marginBottom: "20px" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="Address"
+              variant="outlined"
+              style={{ marginBottom: "20px" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="National ID number"
+              variant="outlined"
+              style={{ marginBottom: "20px" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="Username"
+              variant="outlined"
+              style={{ marginBottom: "20px" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="Password"
+              variant="outlined"
+              style={{ marginBottom: "20px" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="Confirm Password"
+              variant="outlined"
+              style={{ marginBottom: "20px" }}
+            />
+          </Grid>
+        </Grid>
+
+        <Typography variant="body1" style={{ marginBottom: "20px", color: "#9C1C1C" }}>
+          Already have an account? <Link to="/login">Login</Link>
+        </Typography>
+
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ width: "100%", borderRadius: "10px" }}
         >
           Sign In
-        </h2>
-
-
-      {/* <img src="./HealthLabLogo.jpg" alt="My Photo" style = {{position: "absolute",
-width: "500px",
-height: "100px",
-left: "1030px",
-top: "187px"}}
- ></img> */}
-
-        <div className="form-group">
-          <label className="labels" htmlFor="fullname">
-            Full Name           
-          </label>
-          <div className="custom_input">
-            <input style={{
-                width: "350px",
-                height: "25px",
-                left: "1087px",
-                top: "915px",
-                borderRadius: "5px",
-                background: "rgba(0, 0, 0, 0.04)"
-              }}
-              className="details-input form-control"
-              type="text"
-              id="fullname"
-              placeholder=""
-            />
-          </div>
-          <br/>
-        </div>
-    
-
-        <div className="form-group">
-          <label className="labels" htmlFor="email">
-            Email          
-          </label>
-          <div className="custom_input">
-            <input style={{
-                width: "350px",
-                height: "25px",
-                left: "1087px",
-                top: "915px",
-                borderRadius: "5px",
-                background: "rgba(0, 0, 0, 0.04)"
-              }}
-              className="details-input form-control"
-              type="text"
-              id="email"
-              placeholder=""
-            />
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label className="labels" htmlFor="address">
-            Address           
-          </label>
-          <div className="custom_input">
-            <input style={{
-                width: "350px",
-                height: "25px",
-                left: "1087px",
-                top: "915px",
-                borderRadius: "5px",
-                background: "rgba(0, 0, 0, 0.04)"
-              }}
-              className="details-input form-control"
-              type="text"
-              id="address"
-              placeholder=""
-            />
-          </div>
-          <br/>
-        </div>
-
-        <div className="form-group">
-          <label className="labels" htmlFor="id">
-            National ID number           
-          </label>
-          <div className="custom_input">
-            <input style={{
-                width: "350px",
-                height: "25px",
-                left: "1087px",
-                top: "915px",
-                borderRadius: "5px",
-                background: "rgba(0, 0, 0, 0.04)"
-              }}
-              className="details-input form-control"
-              type="text"
-              id="id"
-              placeholder=""
-            />
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label className="labels" htmlFor="username">
-            Username           
-          </label>
-          <div className="custom_input">
-            <input style={{
-                width: "350px",
-                height: "25px",
-                left: "1087px",
-                top: "915px",
-                borderRadius: "5px",
-                background: "rgba(0, 0, 0, 0.04)"
-              }}
-              className="details-input form-control"
-              type="text"
-              id="username"
-              placeholder=""
-            />
-          </div>
-          <br/>
-        </div>
-
-        <div className="form-group">
-          <label className="labels" htmlFor="password">
-            Password           
-          </label>
-          <div className="custom_input">
-            <input style={{
-                width: "350px",
-                height: "25px",
-                left: "1087px",
-                top: "915px",
-                borderRadius: "5px",
-                background: "rgba(0, 0, 0, 0.04)"
-              }}
-              className="details-input form-control"
-              type="text"
-              id="password"
-              placeholder=""
-            />
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label className="labels" htmlFor="space">    
-          </label>
-          <div className="custom_input">
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label className="labels" htmlFor="confirmpassword">
-           Confirm Password           
-          </label>
-          <div className="custom_input">
-            <input style={{
-                width: "350px",
-                height: "25px",
-                left: "1087px",
-                top: "915px",
-                borderRadius: "5px",
-                background: "rgba(0, 0, 0, 0.04)"
-              }}
-              className="details-input form-control"
-              type="text"
-              id="confirmpassword"
-              placeholder=""
-            />
-          </div>
-          <br/>
-        </div>
-
-        <div className="form-group">
-          <label className="labels" htmlFor="space">    
-          </label>
-          <div className="custom_input">
-          </div>
-        </div>
-
-        <div className="form-group">
-         
-          <label className="labels" htmlFor="space"           
-          style={{
-
-fontFamily: 'Inter',
-fontStyle: "normal",
-fontWeight: "500",
-fontSize: "12px",
-lineHeight: "24px",
-
-color: "#9C1C1C"}}> 
-
-          <Link to="/login">Already have an account?</Link>
-          <Routes>
-          <Route path="/login" component={Login} />
-          </Routes>
-          </label>
-
-          <div className="custom_input">
-          </div>
-          <br/>
-        </div>
-
-        <div className="form-group">
-          <label className="labels" htmlFor="space">    
-          </label>
-          <div className="custom_input">
-          </div>
-        </div>
-
-        <Button sx={{variant:'contained' ,color:'#FFFFFF', background:'#101754', width: "150px",
-                height: "35px", borderRadius: "10px"}}>
-          Sign In
         </Button>
-
-        <br />
-        <br />
       </form>
-      <br />
-      <br />
-    </div>
+    </Grid>
   );
 }
 
